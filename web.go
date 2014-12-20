64c0a8aac5d93de8e1f6f0655ca56e6eb76577ac
Jesper Joergensen <jesper@heroku.com>
2014-12-20 10:51:55 -0800

    now a drain

--- web.go
+++ web.go
@@ -1,20 +1,27 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "os"
+	"fmt"
+	"net/http"
+	"os"
+	"bufio"
+	"github.com/bmizerany/lpx"
 )
 
 func main() {
-    http.HandleFunc("/", hello)
-    fmt.Println("listening...")
-    err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
-    if err != nil {
-      panic(err)
-    }
+	http.HandleFunc("/", readMsg)
+	fmt.Println("listening...")
+	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+	if err != nil {
+		panic(err)
+	}
 }
 
-func hello(res http.ResponseWriter, req *http.Request) {
-    fmt.Fprintln(res, "hello, world")
+func readMsg(w http.ResponseWriter, r *http.Request) {
+	lp := lpx.NewReader(bufio.NewReader(r.Body))
+	for lp.Next() {
+		if string(lp.Header().Name) == "router" {
+			fmt.Println("Got router line with "+string(len(lp.Bytes()))+" bytes")
+		}
+	}
 }
