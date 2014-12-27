52b4cc4f640356cf21c2396d1cd4079e5da1e899
Jesper Joergensen <jesper@heroku.com>
2014-12-27 01:13:24 -0800

    factored stuff into other files

---
+++ get_stats.go
@@ -0,0 +1,106 @@
+package main
+	
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/garyburd/redigo/redis"
+	"net/http"
+)
+
+func statsForAllHosts(w http.ResponseWriter, r *http.Request) {
+	c := redisPool.Get()
+	defer c.Close()
+
+	reply, err := redis.Values(c.Do("KEYS", "host:*"))
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		http.Error(w, "oops", http.StatusInternalServerError)
+	}
+
+	// Prepare map for final results
+	allHostsResult := make(map[string]string)
+
+	// Build the results map
+	// TODO: Redis performance can probably be improved here with pipelining or other techniques
+	for _, key := range reply {
+
+		// data comes back from redis untyped, so we need to explictly cast
+		keyBytes := key.([]byte)
+
+		// extract host from key
+		host := string(keyBytes[5:len(keyBytes)])
+
+		// get total count for this host
+		reply2, err := c.Do("GET", string(keyBytes))
+		if err != nil {
+			fmt.Printf("Error: %v\n", err)
+			http.Error(w, "oops", http.StatusInternalServerError)
+			return
+		}
+
+		// store in map
+		allHostsResult[host] = string(reply2.([]byte))
+	}
+
+	js, err := json.Marshal(allHostsResult)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
+
+func bucketDataForHost(w http.ResponseWriter, r *http.Request) {
+	c := redisPool.Get()
+	defer c.Close()
+
+	// Extract hostname from URL path
+	host := r.URL.Path[12:len(r.URL.Path)]
+
+	// Look up all minute buckets for this host
+	reply, err := redis.Values(c.Do("KEYS", host+":*"))
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		http.Error(w, "oops", http.StatusInternalServerError)
+		return
+	}
+
+	// Prepare map for final results
+	values := make(map[string]string)
+
+	// Build the results map
+	// TODO: Redis performance can probably be improved here with pipelining or other techniques
+	for _, key := range reply {
+
+		// data comes back from redis untyped, so we need to explictly cast
+		keyBytes := key.([]byte)
+
+		// extract minute bucket from key
+		minuteBucket := string(keyBytes[len(host)+1:len(keyBytes)])
+
+		// get count for this bucket
+		reply2, err := c.Do("GET", string(keyBytes))
+		if err != nil {
+			fmt.Printf("Error: %v\n", err)
+			http.Error(w, "oops", http.StatusInternalServerError)
+			return
+		}
+
+		// store in map
+		values[minuteBucket] = string(reply2.([]byte))
+	}
+
+	// send back data as JSON
+	js, err := json.Marshal(values)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+
+
+}
---
+++ process_logs.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/bmizerany/lpx"
+	"github.com/kr/logfmt"
+	"net/http"
+	"time"
+)
+
+// This struct and the method below takes care of capturing what we care about
+// from each log line. We pass it to Keith Rarick's logfmt parser and it takes
+// care of the rest.
+type routerLog struct {
+	host string
+}
+
+func (r *routerLog) HandleLogfmt(key, val []byte) error {
+	if string(key) == "host" {
+		r.host = string(val)
+	}
+	return nil
+}
+
+// This is called every time we receive log lines from an app
+func processLogs(w http.ResponseWriter, r *http.Request) {
+	c := redisPool.Get()
+	defer c.Close()
+
+	lp := lpx.NewReader(bufio.NewReader(r.Body))
+	// a single request may contain multiple log lines. Loop over each of them
+	for lp.Next() {
+		// we only care about logs from the heroku router
+		if string(lp.Header().Procid) == "router" {
+			rl := new(routerLog)
+			if err := logfmt.Unmarshal(lp.Bytes(), rl); err != nil {
+				fmt.Printf("Error parsing log line: %v\n", err)
+			} else {
+				timeBucket := timestamp2Bucket(lp.Header().Time)
+				_, err := c.Do("INCR", fmt.Sprintf("%v:%v", rl.host, timeBucket))
+				if err != nil {
+					fmt.Printf("Error running INCR on Redis: %v\n", err)
+				}
+				_, err = c.Do("INCR", "host:"+rl.host)
+				if err != nil {
+					fmt.Printf("Error running INCR on Redis: %v\n", err)
+				}
+				fmt.Printf("%v @ %v: +1\n", rl.host, timeBucket)
+			}
+		}
+	}
+}
+
+// Heroku log lines are formatted according to RFC5424 which is a subset
+// of RFC3339 (RFC5424 is more restrictive).
+// Reference: https://devcenter.heroku.com/articles/logging#log-format
+func timestamp2Bucket(b []byte) int64 {
+	t, err := time.Parse(time.RFC3339, string(b))
+	if err != nil {
+		fmt.Println(err)
+	}
+	return (t.Unix() / 60) * 60
+}
