486ea74d555a6b5bd4940bf60559cd5a939a5f66
Jesper Joergensen <jesper@heroku.com>
2014-12-27 17:36:17 -0800

    Test was outdated and actually failed :-(. Updated now and passes.

--- process_logs_test.go
+++ process_logs_test.go
@@ -7,7 +7,7 @@ import (
 // We use minute buckets. Want to make sure timestamps are properly translated into such buckets
 func TestTimestamp2Bucket(t *testing.T) {
 	in := []byte("2014-12-25T19:02:49.959156+00:00")
-	want := int64(1419534169 / 60)
+	want := int64((1419534169 / 60) * 60)
 	got := timestamp2Bucket(in)
 	if got != want {
 		t.Errorf("TestTimestamp2Bucket(%v) == %v, want %v", in, got, want)
